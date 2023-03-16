b20d760f95e753887a978a43c8e28151de9bf19a
Jack <128088795+dasfledermauss@users.noreply.github.com>
2023-03-16 16:14:59 -0300

    Delete echo "# Wordder" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https: directory

--- echo "# Wordder" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https:/hello.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color A
-cls
-echo Hello, stranger. Ho are you?
-set /p Name= :
-echo Oh, hello %Name%.  Thanks for the visit, teach me a new word.
-set /p Algo= :
-echo Well... %Algo%? Interesting. I'll save it to my database.
-pause
